48a2f8f3ec0b9a305abbd8deec6c8aee5c9592a1
estudm <micha.studer@gmail.com>
2016-04-18 10:16:35 +0200

    motor files added

---
+++ src/motor_pwm.c
@@ -0,0 +1,8 @@
+/*
+ * motor_pwm.c
+ *
+ *  Created on: Apr 18, 2016
+ *      Author: estudm
+ */
+#include "motor_pwm.h"
+
---
+++ src/motor_pwm.h
@@ -0,0 +1,13 @@
+/*
+ * motor_pwm.h
+ *
+ *  Created on: Apr 18, 2016
+ *      Author: estudm
+ */
+
+#ifndef MOTOR_PWM_H_
+#define MOTOR_PWM_H_
+
+
+
+#endif /* MOTOR_PWM_H_ */
